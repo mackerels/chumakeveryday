240191e2e21797116fc52fb290bf4d7db8ca0ec2
421p <andrew.bakal@gmail.com>
2016-11-16 01:02:07 +0200

    temp bitmap

--- Provider/ImageProvider.cs
+++ Provider/ImageProvider.cs
@@ -5,6 +5,18 @@ namespace CoreSandbox.Provider
 {
     public class ImageProvider
     {
-        public static Image GetImage() => Image.FromFile(Configurator.Config.Image);
+        public static Image GetImage()
+        {
+            var original = Image.FromFile(Configurator.Config.Image);
+            var generated = new Bitmap(original.Width, original.Height);
+
+            using (var graph = Graphics.FromImage(generated))
+            {
+                graph.DrawImage(original, 0, 0);
+                graph.Save();
+            }
+
+            return generated;
+        }
     }
 }
